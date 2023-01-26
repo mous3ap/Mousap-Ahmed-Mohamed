37574861c46b8a562bb1a8337801ef3b83538542
mous3ap <mousabahmed000@gmail.com>
2023-01-26 18:52:03 +0200

    add try

--- WindowsFormsApp1/WindowsFormsApp1/Employee.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Employee.cs
@@ -108,7 +108,10 @@ namespace WindowsFormsApp1
             MessageBox.Show(Ex.Message);
         }
         private void DeleteBtn_Click(object sender, EventArgs e)
-    }
+        {
+            try
+            {
+                
 }
     }
 }
