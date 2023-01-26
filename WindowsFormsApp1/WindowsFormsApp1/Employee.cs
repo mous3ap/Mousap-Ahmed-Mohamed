0054768581c3b3467647e252a05b307d14ff354d
mous3ap <mousabahmed000@gmail.com>
2023-01-26 19:22:27 +0200

    add  Con.SetData(Query); add ShowEmp();

--- WindowsFormsApp1/WindowsFormsApp1/Employee.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Employee.cs
@@ -160,6 +160,10 @@ namespace WindowsFormsApp1
                 int Salary = Convert.ToInt32(DailySalTb.Text);
                 string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
                 Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
+                Con.SetData(Query);
+                ShowEmp();
+
+
 
 
             }
