cf4d97eab16e04468536b1cf50b72ac1b055545b
mous3ap <mousabahmed000@gmail.com>
2023-01-26 18:38:52 +0200

    add private void label3_Click(object sender, EventArgs e)

--- WindowsFormsApp1/WindowsFormsApp1/Employee.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Employee.cs
@@ -35,7 +35,14 @@ namespace WindowsFormsApp1
                 }
                 private void textBox1_TextChanged(object sender, EventArgs
             }
+            {
 
+            }
+
+            private void label3_Click(object sender, EventArgs e)
+            {
+
+            }
             private void pictureBox1_Click(object sender, EventArgs e)
         {
 
