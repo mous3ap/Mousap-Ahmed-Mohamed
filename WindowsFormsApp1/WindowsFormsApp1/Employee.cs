730c3a0f68e8696394c31f2abf2d00d24a1f4cd1
mous3ap <mousabahmed000@gmail.com>
2023-01-26 19:08:09 +0200

    add Con.SetData(Query); add showEmp

--- WindowsFormsApp1/WindowsFormsApp1/Employee.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Employee.cs
@@ -127,6 +127,12 @@ namespace WindowsFormsApp1
                     int Salary = Convert.ToInt32(DailySalTb.Text);
                     string Query = "Delete from EmployeeTb1 where Empid= {0}";
                     Query = string.Format(Query, key);
+                    Con.SetData(Query);
+                    ShowEmp();
+
+
+            }
+
 
 
                 }
