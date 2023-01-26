0a0136ba536658f09844be2c9c922cef2eda24cb
mous3ap <mousabahmed000@gmail.com>
2023-01-26 19:28:04 +0200

    add catch (Exception Ex)

--- WindowsFormsApp1/WindowsFormsApp1/Employee.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Employee.cs
@@ -136,36 +136,41 @@ namespace WindowsFormsApp1
                     DepCb.SelectedIndex = -1;
                 }
             }
-             catch (Exception Ex)
-            }
+            catch (Exception Ex)
             {
                 MessageBox.Show(Ex.Message);
             }
-            private void UpdateBtn_Click(object sender, EventArgs e)
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            try
             {
-                try
+                if (EmpNameTb.Text == ""  GenCb.SelectedIndex == -1  DepCb.SelectedIndex == -1 || DailySalTb.Text == "")
                 {
-                    if (EmpNameTb.Text == ""  GenCb.SelectedIndex == -1  DepCb.SelectedIndex == -1 || DailySalTb.Text == "")
-                {
-                MessageBox.Show("missing data!!!");
-            }
+                    MessageBox.Show("missing data!!!");
+                }
                 else
+                {
+                    string Name = EmpNameTb.Text;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
+                    string DOB = DOBTb.Value.Date.ToString();
+                    string JDate = JDateTb.Value.Date.ToString();
+                    int Salary = Convert.ToInt32(DailySalTb.Text);
+                    string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
+                    Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
+                    Con.SetData(Query);
+                    ShowEmp();
+                    MessageBox.Show("Emoloyee Updated!!!");
+                    EmpNameTb.Text = "";
+                    DailySalTb.Text = "";
+                    GenCb.SelectedIndex = -1;
+                    DepCb.SelectedIndex = -1;
+                }
+            }
+            catch (Exception Ex)
             {
-                string Name = EmpNameTb.Text;
-                string Gender = GenCb.SelectedItem.ToString();
-                int Dep =
-                Convert.ToInt32(DepCb.SelectedValue.ToString());
-                string DOB = DOBTb.Value.Date.ToString();
-                string JDate = JDateTb.Value.Date.ToString();
-                int Salary = Convert.ToInt32(DailySalTb.Text);
-                string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
-                Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
-                Con.SetData(Query);
-                ShowEmp();
-                MessageBox.Show("Emoloyee Updated!!!");
-                EmpNameTb.Text = "";
-                DailySalTb.Text = "";
-                GenCb.SelectedIndex = -1;
 
 
 
