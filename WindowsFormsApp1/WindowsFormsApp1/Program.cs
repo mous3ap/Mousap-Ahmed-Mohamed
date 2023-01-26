264f05f3cab44ae946f4bfbf6a969795ab1ad19e
mous3ap <mousabahmed000@gmail.com>
2023-01-26 03:38:27 +0200

    create a new class (functions)

--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Empolyees());
+            Application.Run(new Department());
         }
     }
 
