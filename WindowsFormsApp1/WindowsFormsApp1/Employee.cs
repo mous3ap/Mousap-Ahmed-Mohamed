a00e1b47ee3ab9accc402f185351eb23c213a42a
mous3ap <mousabahmed000@gmail.com>
2023-01-26 19:23:24 +0200

    add   EmpNameTb.Text = "";

--- WindowsFormsApp1/WindowsFormsApp1/Employee.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Employee.cs
@@ -164,6 +164,7 @@ namespace WindowsFormsApp1
                 ShowEmp();
                 MessageBox.Show("Emoloyee Updated!!!");
                 EmpNameTb.Text = "";
+                EmpNameTb.Text = "";
 
 
 
