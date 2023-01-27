f2685c69d70497329253e41536865bf2b227eefc
mous3ap <mousabahmed000@gmail.com>
2023-01-28 00:01:00 +0200

    add int

---
+++ WindowsFormsApp1/dbo.SalaryTbl.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SalaryTbl] (
+    [SCode]      INT           IDENTITY (1, 1) NOT NULL,
+    [Employee]   INT           NOT NULL,
+    [Attendance] INT           NOT NULL,
+    [Period]     VARCHAR (100) NOT NULL,
+    [Amount]     INT           NOT NULL,
+    [PayDate]    DATE          NOT NULL,
+    PRIMARY KEY CLUSTERED ([SCode] ASC),
+    CONSTRAINT [FK3] FOREIGN KEY ([Employee]) REFERENCES [dbo].[EmployeeTbl] ([EmpId])
+);
+
